e54d33f82c152e2f6f9c2540ee95b3f5a6796ab5
prospergreg <gelward@prospercanada.org>
2025-10-06 16:13:23 -0400

    update

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Order matters if you rely on globals—keep this sequence
+import "../i18n/i18n-toggle.js";
+import "../i18n/bootstrap-i18n.js";
+import "../i18n/i18n.js";
+import "../i18n/doit.js";
+import "../i18n/dynamic-i18n.js";
